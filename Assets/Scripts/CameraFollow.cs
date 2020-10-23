b2a1d65c267e2c014415cb1b4292d28cc311e763
decimo1 <konstantinzehnter@protonmail.com>
2020-10-23 14:06:17 +0200

    First commit

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Vector3 offset;
+    public Transform player;
+
+    // Start is called before the first frame update
+    void Start(){
+        offset = new Vector3(0, 5, -16); 
+    }
+
+    // Update is called once per frame
+    void Update(){
+        Vector3 rotatedOffset = player.rotation * offset;
+        transform.position = player.position + rotatedOffset;
+        transform.rotation = player.rotation;
+    }
+}
---
+++ Assets/Scripts/Prefab.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prefab : MonoBehaviour
+{
+    public Transform myPrefab;
+    ArrayList list;
+    Vector3 vector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        list = new ArrayList();
+        for (int i=0; i<5; i++) {
+            do {
+                vector = new Vector3(UnityEngine.Random.Range(-5,5),1,UnityEngine.Random.Range(-5,5));
+            } while (list.Contains(vector));
+            Instantiate(myPrefab,vector,new Quaternion(0,0,0,1));
+            list.Add(vector);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
