8a9768dd8b40379297da4e9d72e701a62c9a7003
decimo1 <konstantinzehnter@protonmail.com>
2020-10-29 01:22:56 +0100

    Menu-Scene, buttons, renames

--- Assets/Scripts/Collection.cs
+++ Assets/Scripts/Collection.cs
@@ -6,16 +6,6 @@ public class Collection : MonoBehaviour
 {
     private int score = 0;
 
-    // Start is called before the first frame update
-    void Start(){
-        
-    }
-
-    // Update is called once per frame
-    void Update(){
-        
-    }
-
     void OnTriggerEnter(Collider other) {
         if (other.gameObject.CompareTag("Player")) {
             score++;
