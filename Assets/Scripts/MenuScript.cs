261a4c1dafd6066f9c7b1a3ee2f3e72a9fefbbb8
decimo1 <konstantinzehnter@protonmail.com>
2020-11-02 00:22:22 +0100

    one scene, button functions

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+	public GameObject Menu;
+	
+	void Update(){
+		if (Input.GetKey(KeyCode.Escape)) {
+			if (Menu.isActiveAndEnabled == false) {
+				Menu.SetActive(true);
+				Time.timeScale = 0;
+			} else {
+				Menu.SetActive(false);
+				Time.timeScale = 1;
+			}
+		}
+	}
+
+	public void RestartClick(){
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
+
+	public void ReturnClick(){
+		Menu.SetActive(false);
+		Time.timeScale = 1;
+	}	
+}
