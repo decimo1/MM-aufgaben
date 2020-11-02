6b73cd7d60e6d3753aa18418d1eddf7e61b81fbf
decimo1 <konstantinzehnter@protonmail.com>
2020-11-02 22:28:40 +0100

    VFX for spheres, Menu opening fixed

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -6,15 +6,17 @@ using UnityEngine.SceneManagement;
 public class MenuScript : MonoBehaviour
 {
 	public GameObject Menu;
+	int counter;
 	
 	void Update(){
-		if (Input.GetKey(KeyCode.Escape)) {
-			if (true) {
-				Menu.SetActive(true);
-				Time.timeScale = 0;
-			} else {
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+			counter++;
+			if (counter%2==1) {
 				Menu.SetActive(false);
 				Time.timeScale = 1;
+			} else {
+				Menu.SetActive(true);
+				Time.timeScale = 0;
 			}
 		}
 	}
