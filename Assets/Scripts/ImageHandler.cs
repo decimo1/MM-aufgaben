0144b5c2fdcb2617b84a7ff9c3154b957deb4814
decimo1 <konstantinzehnter@protonmail.com>
2020-11-11 18:00:13 +0100

    all AR stuff, gradle

--- Assets/Scripts/ImageHandler.cs
+++ Assets/Scripts/ImageHandler.cs
@@ -15,6 +15,15 @@ public class ImageHandler : MonoBehaviour
 	}
     }
 
-    //void OnEnable() => _trackedImageManager.trackedImagesChanged += OnChanged;
-    //void OnDisable() => _trackedImageManager.trackedImagesChanged -= OnChanged;
+    void OnEnable() => _trackedImageManager.trackedImagesChanged += OnChanged;
+    void OnDisable() => _trackedImageManager.trackedImagesChanged -= OnChanged;
+
+    void OnChanged(ARTrackedImagesChangedEventArgs eventArgs){
+	foreach(var trackedImage in eventArgs.added){
+            var minLocalScalar = Mathf.Min(trackedImage.size.x, trackedImage.size.y) / 2;
+            trackedImage.transform.localScale = new Vector3(minLocalScalar, minLocalScalar, minLocalScalar);
+	    _content.transform.parent = trackedImage.transform;
+	    _content.transform.localScale = Vector3.one;
+        }
+    }
 }
--- Assets/Scripts/SphereCreation.cs
+++ Assets/Scripts/SphereCreation.cs
@@ -7,23 +7,19 @@ public class SphereCreation : MonoBehaviour
     public Transform myPrefab;
     ArrayList list;
     Vector3 vector;
+    Vector3 scaleVector;
 
     // Start is called before the first frame update
     void Start()
     {
+	//scaleVector = new Vector3(0.01f,0.01f,0.01f);
         list = new ArrayList();
         for (int i=0; i<5; i++) {
             do {
-                vector = new Vector3(UnityEngine.Random.Range(-5,5),1,UnityEngine.Random.Range(-5,5));
+                vector = new Vector3(UnityEngine.Random.Range(-5,5)/100,1,UnityEngine.Random.Range(-5,5)/100);
             } while (list.Contains(vector));
             Instantiate(myPrefab,vector,new Quaternion(0,0,0,1));
             list.Add(vector);
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 }
