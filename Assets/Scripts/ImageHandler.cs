2589827ac2da493c64342eb9b1cafdb802d8cbff
decimo1 <konstantinzehnter@protonmail.com>
2020-11-11 19:32:54 +0100

    ImageHandler fixed, first time done

--- Assets/Scripts/ImageHandler.cs
+++ Assets/Scripts/ImageHandler.cs
@@ -6,9 +6,8 @@ using UnityEngine.XR.ARFoundation;
 public class ImageHandler : MonoBehaviour
 {
     [SerializeField] ARTrackedImageManager _trackedImageManager;
-   // [SerializeField] GameObject _player;
-   // [SerializeField] GameObject _surface;
     [SerializeField] GameObject _parent;
+
     private void Start()
     {
         if(!_trackedImageManager) {
@@ -23,11 +22,7 @@ public class ImageHandler : MonoBehaviour
 	foreach(var trackedImage in eventArgs.added){
             var minLocalScalar = Mathf.Min(trackedImage.size.x, trackedImage.size.y) / 2;
             trackedImage.transform.localScale = new Vector3(minLocalScalar, minLocalScalar, minLocalScalar);
-	    _parent.transform.parent = trackedImage.transform; 
-	    //_surface.transform.parent = trackedImage.transform;
-	    //_surface.transform.localScale = Vector3.one;
-	    //_player.transform.parent = trackedImage.transform;
-	    //_player.transform.localScale = Vector3.one;
+	    _parent.transform.parent = trackedImage.transform;
         }
     }
 }
