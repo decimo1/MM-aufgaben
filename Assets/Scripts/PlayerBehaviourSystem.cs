333331620710e984e3e455e881be6c6eb070e453
decimo1 <konstantinzehnter@protonmail.com>
2020-11-21 20:50:23 +0100

    trying structs and systems

---
+++ Assets/Scripts/PlayerBehaviourSystem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.Mathematics;
+
+public class PlayerBehaviourSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+       Entities.ForEach((ref PlayerComponent comp, ref Translation trans, ref Rotation rot) =>
+       {
+          comp.rotationAngle+=Input.GetAxis("Horizontal");
+          float3 targetDirection = new float3(math.sin(comp.rotationAngle), 0, math.cos(comp.rotationAngle));
+          rot.value = quaternion.LookRotationSafe(targetDirection/*forward*/, comp.rotationAngle/*up*/);
+       }).Schedule();
+    }
+}
